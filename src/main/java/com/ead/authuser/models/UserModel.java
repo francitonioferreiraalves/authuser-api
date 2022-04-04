449a9a2c70ea929d214b01116cbe5e6e18a60564
francitonioferreiraalves <francitonio@gmail.com>
2022-04-04 13:38:38 -0300

    criando classe instrutor

--- src/main/java/com/ead/authuser/models/UserModel.java
+++ src/main/java/com/ead/authuser/models/UserModel.java
@@ -38,40 +38,51 @@ public class UserModel extends RepresentationModel<UserModel> implements Seriali
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private UUID userId;
+
 	@Column(nullable = false, unique = true, length = 50)
 	private String username;
+
 	@Column(nullable = false, unique = true, length = 50)
 	private String email;
+
 	@Column(nullable = false, length = 255)
 	@JsonIgnore
 	private String password;
+
 	@Column(nullable = false, length = 150)
 	private String fullName;
+
 	@Column(nullable = false)
 	@Enumerated(EnumType.STRING)
 	private UserStatus userStatus;
+
 	@Column(nullable = false)
 	@Enumerated(EnumType.STRING)
 	private UserType userType;
+
 	@Column(length = 20)
 	private String phoneNumber;
+
 	@Column(length = 20)
 	private String cpf;
+
 	@Column
 	private String imageUrl;
+
 	@Column(nullable = false)
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
 	private LocalDateTime creationDate;
+
 	@Column(nullable = false)
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
 	private LocalDateTime lastUpdateDate;
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
-    private Set<UserCourseModel> usersCourses;
 
-    public UserCourseModel convertToUserCourseModel(UUID courseId){
-        return new UserCourseModel(null, courseId, this);
-    }
+	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+	@OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+	private Set<UserCourseModel> usersCourses;
 
+	public UserCourseModel convertToUserCourseModel(UUID courseId) {
+		return new UserCourseModel(null, courseId, this);
+	}
 
 }
--- src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
+++ src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
@@ -9,8 +9,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.ead.authuser.models.UserCourseModel;
 import com.ead.authuser.models.UserModel;
+import com.ead.authuser.repositories.UserCourseRepository;
 import com.ead.authuser.repositories.UserRepository;
 import com.ead.authuser.services.UserService;
 
@@ -19,6 +22,9 @@ public class UserServiceImpl implements UserService {
 
 	@Autowired
 	UserRepository userRepository;
+
+	@Autowired
+	UserCourseRepository userCourseRepository;
 
 	@Override
 	public List<UserModel> findAll() {
@@ -30,10 +36,15 @@ public class UserServiceImpl implements UserService {
 		return userRepository.findById(userId);
 	}
 
+	@Transactional
 	@Override
 	public void delete(UserModel userModel) {
+		List<UserCourseModel> userCourseModelList = userCourseRepository
+				.findAllUserCourseIntoUser(userModel.getUserId());
+		if (!userCourseModelList.isEmpty()) {
+			userCourseRepository.deleteAll(userCourseModelList);
+		}
 		userRepository.delete(userModel);
-
 	}
 
 	@Override
